90ae436234d961d2a94d0ebd83a59b68b68244b7
Brittany Brimley <brittanybrimley@brittanys-mbp.lan>
2023-07-17 11:30:30 -0600

    set up localStorage functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,29 @@
-var dateEl = $('#currentDay');
-
-
 $(function () {
+  var dateEl = $('#currentDay');
+  var timeBlock = $('.time-block')
+  var saveButton = $(".saveBtn")
+
   var date = dayjs().format('MMM D, YYYY')
   dateEl.text(date)
 
-  var currentHour = dayjs().hour()
-  var timeBlock = $('.time-block')
-
-  timeBlock.each(function() {
+  timeBlock.each(function () {
+    var currentHour = dayjs().hour()
     var id = Number($(this).attr("id").slice(5))
-    if(id < currentHour) {
+    if (id < currentHour) {
       $(this).children(".description").attr("class", "col-8 col-md-10 description past")
-    } else if(id > currentHour) {
+    } else if (id > currentHour) {
       $(this).children(".description").attr("class", "col-8 col-md-10 description future")
-    } else {
+    } else if (id === currentHour) {
       $(this).children(".description").attr("class", "col-8 col-md-10 description present")
     }
   })
 
+  saveButton.on("click", function (event) {
+    event.preventDefault();
 
+    var value = $(this).siblings(".description").val()
+    var key = $(this).parent().attr("id")
+    localStorage.setItem(key, JSON.stringify(value))
+  })
 
 });
