cc70e02244eabd115cf5c15699b3014d1f64c23c
Brittany Brimley <brittanybrimley@brittanys-mbp.lan>
2023-07-17 11:22:59 -0600

    set up color coding functionallity

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,22 @@ var dateEl = $('#currentDay');
 
 
 $(function () {
-  var date = dayjs().format('MMM, D, YYYY')
+  var date = dayjs().format('MMM D, YYYY')
   dateEl.text(date)
+
+  var currentHour = dayjs().hour()
+  var timeBlock = $('.time-block')
+
+  timeBlock.each(function() {
+    var id = Number($(this).attr("id").slice(5))
+    if(id < currentHour) {
+      $(this).children(".description").attr("class", "col-8 col-md-10 description past")
+    } else if(id > currentHour) {
+      $(this).children(".description").attr("class", "col-8 col-md-10 description future")
+    } else {
+      $(this).children(".description").attr("class", "col-8 col-md-10 description present")
+    }
+  })
 
 
 
